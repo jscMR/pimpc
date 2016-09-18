99dcbb49bc899049fe3e1438e84ddd594bff5ef0
Jorge  Serrano <jorge.serrano@bq.com>
2016-09-18 19:59:44 +0200

    added more buttons

--- pimpc.py
+++ pimpc.py
@@ -7,10 +7,20 @@ from signal import pause
 
 pygame.mixer.init()
 
-button = Button(2)
+# Buttons
+button_drum = Button(2)
+button_snare = Button(3)
+button_hihat = Button(4)
+
+# Samples
 drum = Sound("samples/factory/Bassdrum.wav")
 snare_one = Sound('samples/factory/Snare 1.wav')
+hit_hat = Sound('samples/factory/Open Hat.wav')
 
-button.when_pressed = drum.play
+
+# Press buttons
+button_drum.when_pressed = drum.play
+button_snare.when_pressed = snare_one.play
+button_hihat.when_pressed = hit_hat.play
 
 pause()
