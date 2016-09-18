c039c9c691d5e6c3efd0f4250245549f897c12cc
Jorge  Serrano <jorge.serrano@bq.com>
2016-09-18 19:53:03 +0200

    test button

--- pimpc.py
+++ pimpc.py
@@ -2,11 +2,15 @@
 
 import pygame.mixer
 from pygame.mixer import Sound
+from gpiozero import Button
+from signal import pause
 
 pygame.mixer.init()
 
+button = Button(2)
 drum = Sound("samples/factory/Bassdrum.wav")
 snare_one = Sound('samples/factory/Snare 1.wav')
 
-while True:
-    drum.play()
+button.when_pressed = drum.play
+
+pause()
